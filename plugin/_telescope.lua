8ff4309bc33fa4b37fb72371e5948a6eda401f2a
Harry Cramer <hcramer@crossbeam.com>
2021-12-01 12:16:58 -0500

    Set default view to vertical.

--- plugin/_telescope.lua
+++ plugin/_telescope.lua
@@ -2,6 +2,7 @@ local actions = require('telescope.actions')
 require('telescope').setup{
   defaults = {
     hidden = true,
+    layout_strategy = 'vertical',
     file_ignore_patterns = { "node_modules", "package%-lock.json" },
     mappings = {
       i = {
@@ -11,13 +12,12 @@ require('telescope').setup{
   }
 }
 
-nnoremap('<c-j>', "<cmd>lua require('telescope.builtin').find_files{ layout_strategy = 'vertical', find_command = {'rg', '--files', '--hidden', '-g', '!node_modules/**'}}<cr>")
-nnoremap('<c-f>', "<cmd>lua require('telescope.builtin').live_grep({ layout_strategy = 'vertical', hidden = true })<cr>")
+nnoremap('<c-j>', "<cmd>lua require('telescope.builtin').find_files{ find_command = {'rg', '--files', '--hidden', '-g', '!node_modules/**'}}<cr>")
+nnoremap('<c-f>', "<cmd>lua require('telescope.builtin').live_grep({ hidden = true })<cr>")
 nnoremap('<c-g>', "<cmd>lua require('telescope.builtin').file_browser({ hidden = true })<cr>")
 nnoremap('<c-b>', "<cmd>lua require('telescope.builtin').buffers({ hidden = true })<cr>")
 
 -- Telescope mappings start with s
-
 -- Find current word under cursor in project
 vim.cmd[[
   nnoremap <expr> <leader>sf ':Telescope live_grep<cr>' . expand('<cword>')
