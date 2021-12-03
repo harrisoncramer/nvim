ba6ce9845f0d4659e061151fb6f84d82a31e7666
Harry Cramer <hcramer@crossbeam.com>
2021-12-02 20:43:24 -0500

    added conjure testing hotkey

--- plugin/_conjure.lua
+++ plugin/_conjure.lua
@@ -3,3 +3,12 @@ vim.cmd[[
   let g:conjure#mapping#doc_word = v:false
   let g:conjure#mapping#def_word= v:false
 ]]
+
+-- Evaluate buffer again and run the current test.
+nnoremap('<localleader>T', ':ConjureEvalRootForm<CR> <bar> :ConjureCljRunCurrentTest<CR>')
+
+-- , e b evaluates the current buffer
+-- , e f evaluate the code in the file (from the file system)
+-- , e e evaluate the current expression
+-- , e r evaluate top level form (root)
+-- , e ! evaluate current form and replace with result
