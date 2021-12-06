5a317e2a511939808293e7f5bcc6f532b1c0621e
Harry Cramer <hcramer@crossbeam.com>
2021-12-06 15:18:39 -0500

    Added priority for sign column.

--- plugin/_nvim_lspconfig.lua
+++ plugin/_nvim_lspconfig.lua
@@ -44,7 +44,8 @@ end
 -- Hide inline diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false
+        virtual_text = false,
+        severity_sort = true
     }
 )
 
