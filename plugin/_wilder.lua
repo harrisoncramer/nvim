32358af7ca6c6b98dbea4e86cdd08c0970fc05b4
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-10 17:59:35 -0500

    Configured wilder.

--- plugin/_wilder.lua
+++ plugin/_wilder.lua
@@ -1,9 +1,34 @@
 -- Key bindings can be changed, see below
 vim.cmd [[
   call wilder#setup({'modes': [':', '/', '?']})
-  call wilder#set_option('renderer', wilder#popupmenu_renderer({
+
+call wilder#set_option('renderer', wilder#popupmenu_renderer({
       \ 'highlighter': wilder#basic_highlighter(),
+      \ 'highlights': {
+      \   'accent': wilder#make_hl('WilderAccent', 'Pmenu', [{}, {}, {'foreground': '#f4468f'}]),
+      \ },
+      \ 'left': [
+      \   ' ', wilder#popupmenu_devicons(),
+      \ ],
+      \ 'right': [
+      \   ' ', wilder#popupmenu_scrollbar(),
+      \ ],
       \ }))
+
+
+  call wilder#set_option('pipeline', [
+  \   wilder#branch(
+  \     wilder#cmdline_pipeline({
+  \       'language': 'python',
+  \       'fuzzy': 1,
+  \     }),
+  \     wilder#python_search_pipeline({
+  \       'pattern': wilder#python_fuzzy_pattern(),
+  \       'sorter': wilder#python_difflib_sorter(),
+  \       'engine': 're',
+  \     }),
+  \   ),
+  \ ])
 
   call wilder#setup({
     \ 'modes': [':'],
