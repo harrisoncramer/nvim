6914c89517e173030db6c8d4fe466082966ee646
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-11 12:06:08 -0500

    added diffview

--- plugin/_git.lua
+++ plugin/_git.lua
@@ -38,30 +38,3 @@ end
 
 vim.cmd [[ command! ToggleGLog lua ToggleGLog() ]]
 
--- Diffing and Conflicts
-local last_tabpage = vim.api.nvim_get_current_tabpage()
-
-function M.DiffviewToggle()
-    local lib = require'diffview.lib'
-    local view = lib.get_current_diffview()
-    if view then
-        -- Current tabpage is a Diffview: go to previous tabpage
-        vim.api.nvim_set_current_tabpage(last_tabpage)
-    else
-        -- We are not in a Diffview: save current tabpagenr and go to a Diffview.
-        last_tabpage = vim.api.nvim_get_current_tabpage()
-        if #lib.views > 0 then
-            -- An open Diffview exists: go to that one.
-            vim.api.nvim_set_current_tabpage(lib.views[1].tabpage)
-        else
-            -- No open Diffview exists: Open a new one
-            vim.cmd(":DiffviewOpen")
-        end
-    end
-end
-
--- Full Diffview (plugin)
-remap {'n', '<leader>gd', ':DiffviewFileHistory<CR>'}
-remap {'n', '<leader>gD', ":lua require('_git').DiffviewToggle()<CR>"}
-
-return M
