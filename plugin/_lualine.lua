13a1a1f0c7f97abef2386beb327d621cb51ed0bc
Harry Cramer <hcramer@crossbeam.com>
2021-12-16 19:48:39 -0500

    Configured lualine for full path + gitsigns

--- plugin/_lualine.lua
+++ plugin/_lualine.lua
@@ -1,13 +1,14 @@
 require('lualine').setup({
     options = {theme = 'gruvbox-material'},
     sections = {
+        lualine_a = {'branch'},
+        lualine_b = {'diagnostics', 'diff'},
+        lualine_c = {"vim.fn.expand('%')"},
         lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {}
+        lualine_y = {'progress'}
     }
 })
 
 vim.cmd [[
   au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
 ]]
-
