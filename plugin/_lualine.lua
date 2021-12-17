24d8e4dbdd51fb658d0b2d725603b0b588523fba
Harry Cramer <hcramer@crossbeam.com>
2021-12-17 10:00:20 -0500

    Added clock to lualine.

--- plugin/_lualine.lua
+++ plugin/_lualine.lua
@@ -5,10 +5,19 @@ require('lualine').setup({
         lualine_b = {'diagnostics', 'diff'},
         lualine_c = {"vim.fn.expand('%')"},
         lualine_x = {'filetype'},
-        lualine_y = {'progress'}
+        lualine_y = {'os.date("%I:%M:%S", os.time())'},
+        lualine_z = {}
     }
 })
 
 vim.cmd [[
   au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
 ]]
+
+if _G.Statusline_timer == nil then
+    _G.Statusline_timer = vim.loop.new_timer()
+else
+    _G.Statusline_timer:stop()
+end
+_G.Statusline_timer:start(0, 1000, vim.schedule_wrap(
+                              function() vim.api.nvim_command('redrawstatus') end))
