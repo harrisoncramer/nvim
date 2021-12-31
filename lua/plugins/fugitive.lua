656f724263b50f54255f495c854feccfbe435ddb
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-31 13:17:17 -0500

    Added silent commit ability.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ M.setup = function(remap)
 	remap({ "n", "<leader>gl", ':lua require("plugins.fugitive").ToggleGLog()<CR>' })
 	remap({ "n", "<leader>gP", ":Git push --quiet<cr>" })
 	remap({ "n", "<leader>go", ":!git open<cr><cr>" })
+	vim.cmd([[ :ca x silent x ]])
 end
 
 M.ToggleGStatus = function()
