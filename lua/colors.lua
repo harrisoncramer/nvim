b8caefd575ffe918711b71d462e10af031df9f34
Harrison Cramer <harrisoncramer@gmail.com>
2022-01-02 10:21:27 -0500

    Added conditional loading of Kanagawa

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,23 +1,30 @@
-vim.cmd('set termguicolors " this variable must be enabled for colors to be applied properly')
+vim.cmd("set termguicolors")
 
-require("kanagawa").setup({
-	undercurl = true, -- enable undercurls
-	commentStyle = "italic",
-	functionStyle = "NONE",
-	keywordStyle = "italic",
-	statementStyle = "NONE",
-	typeStyle = "NONE",
-	variablebuiltinStyle = "italic",
-	specialReturn = true, -- special highlight for the return keyword
-	specialException = true, -- special highlight for exception handling keywords
-	transparent = false, -- do not set background color
-	colors = {},
-	overrides = {},
-})
+local kanagawa_status_ok, kanagawa = pcall(require, "kanagawa")
+if kanagawa_status_ok then
+	kanagawa.setup({
+		undercurl = true, -- enable undercurls
+		commentStyle = "italic",
+		functionStyle = "NONE",
+		keywordStyle = "italic",
+		statementStyle = "NONE",
+		typeStyle = "NONE",
+		variablebuiltinStyle = "italic",
+		specialReturn = true, -- special highlight for the return keyword
+		specialException = true, -- special highlight for exception handling keywords
+		transparent = false, -- do not set background color
+		colors = {},
+		overrides = {},
+	})
 
-vim.cmd("colorscheme kanagawa")
-vim.cmd([[
+	vim.cmd("colorscheme kanagawa")
+	vim.cmd([[
   :hi NonText guifg=bg
   :set signcolumn=yes
 ]])
-require("nvim-web-devicons").setup({})
+end
+
+local nvim_web_devicons_status_ok, nvim_web_devicons = pcall(require, "null-ls")
+if nvim_web_devicons_status_ok then
+	nvim_web_devicons.setup({})
+end
