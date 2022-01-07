b0cf96624988c7d26d2d36cd42e79c3def8921e5
Harry Cramer <hcramer@crossbeam.com>
2022-01-07 11:18:10 -0500

    Added config for harpoon.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,7 +56,7 @@ packer.startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 		config = setup("plugins.telescope", "telescope"),
 	})
-  use({ "ThePrimeagen/harpoon", config = setup("plugins.harpoon")})
+  use({ "ThePrimeagen/harpoon", config = setup("plugins.harpoon", "harpoon")})
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use("tpope/vim-dispatch")
 	use("tpope/vim-repeat")
