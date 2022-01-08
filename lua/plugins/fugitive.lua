79ad8b3c7b1fc085436ab171d88c548e46f04265
Harrison Cramer <harrisoncramer@gmail.com>
2022-01-08 10:47:36 -0500

    Replaced other mappings.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,23 +1,24 @@
 local f = require("functions")
 
+local toggleStatus = function()
+	vim.cmd([[
+  if buflisted(bufname('.git/index'))
+      bd .git/index
+  else
+      Git
+  endif
+]])
+end
+
 return {
 	setup = function(remap)
-		remap({ "n", "<leader>gs", ':lua require("plugins.fugitive").toggleStatus()<CR>' })
-    if f.getOS() == 'Linux' then
-		  remap({ "n", "<leader>gP", ":! git push <cr>" })
-		  remap({ "n", "<leader>go", ":! git open <cr>" })
-    else
-		  remap({ "n", "<leader>gP", ":Git push<cr>" })
-		  remap({ "n", "<leader>go", ":Git open<cr>" })
-    end
-	end,
-	toggleStatus = function()
-		vim.cmd([[
-    if buflisted(bufname('.git/index'))
-        bd .git/index
-    else
-        Git
-    endif
-  ]])
+		vim.keymap.set("n", "<leader>gs", toggleStatus)
+		if f.getOS() == "Linux" then
+			remap({ "n", "<leader>gP", ":! git push <cr>" })
+			remap({ "n", "<leader>go", ":! git open <cr>" })
+		else
+			remap({ "n", "<leader>gP", ":Git push<cr>" })
+			remap({ "n", "<leader>go", ":Git open<cr>" })
+		end
 	end,
 }
