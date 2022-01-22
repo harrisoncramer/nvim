b6abee0b35f2fa393594ef3c97d2cc90dbb82757
Harrison Cramer <harrisoncramer@gmail.com>
2022-01-21 20:02:36 -0500

    Fixed fugitive.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,23 +1,39 @@
 local f = require("functions")
 
+local toggleStatus = function()
+	local ft = vim.bo.filetype
+	if ft == "fugitive" then
+		vim.api.nvim_command("bd")
+	else
+		vim.api.nvim_command("Git")
+	end
+end
+
+local gitPush = function()
+	local isSubmodule = vim.fn.trim(vim.fn.system("git rev-parse --show-superproject-working-tree"))
+	if isSubmodule == "" then
+		if f.getOS() == "Linux" then
+			vim.api.nvim_command("Git push")
+		else
+			vim.api.nvim_command("! git push")
+		end
+	else
+		vim.fn.confirm("Push to origin/main branch for submodule?")
+		vim.api.nvim_command("! git push origin HEAD:main")
+	end
+end
+
+local gitOpen = function()
+	vim.api.nvim_command("! git open")
+end
+
 return {
 	setup = function(remap)
-		remap({ "n", "<leader>gs", ':lua require("plugins.fugitive").toggleStatus()<CR>' })
-    if f.getOS() == 'Linux' then
-		  remap({ "n", "<leader>gP", ":! git push <cr>" })
-		  remap({ "n", "<leader>go", ":! git open <cr>" })
-    else
-		  remap({ "n", "<leader>gP", ":Git push<cr>" })
-		  remap({ "n", "<leader>go", ":Git open<cr>" })
-    end
+		remap({ "n", "<leader>gs", ":lua require('plugins.fugitive').toggleStatus()<CR>" })
+		remap({ "n", "<leader>gP", ":lua require('plugins.fugitive').gitPush()<CR>" })
+		remap({ "n", "<leader>go", ":lua require('plugins.fugitive').gitOpen()<CR>" })
 	end,
-	toggleStatus = function()
-		vim.cmd([[
-    if buflisted(bufname('.git/index'))
-        bd .git/index
-    else
-        Git
-    endif
-  ]])
-	end,
+	gitPush = gitPush,
+	gitOpen = gitOpen,
+	toggleStatus = toggleStatus,
 }
