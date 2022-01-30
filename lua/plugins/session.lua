2c5d3ed7e63a3b3bf990ab64f6ecffed1b6d1184
Harrison Cramer <harrisoncramer@gmail.com>
2022-01-30 16:39:31 -0500

    Fixed conflict w/ nvim-tree

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -2,19 +2,19 @@ local get_branch_name = require("functions").get_branch_name
 
 local opts = {
 	log_level = "info",
-	auto_session_enable_last_session = true,
-	auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/" .. get_branch_name() .. "/",
-	auto_session_enabled = true,
-	auto_save_enabled = true,
-	auto_restore_enabled = true,
 	auto_session_suppress_dirs = nil,
-	pre_save_cmds = { "lua require'nvim-tree'.setup()", "tabdo NvimTreeClose" },
 }
 
 return {
 	setup = function()
 		local branch = get_branch_name()
-		print(branch)
-		require("auto-session").setup(opts)
+		if branch then
+			opts.auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/" .. get_branch_name() .. "/"
+			opts.auto_session_enable_last_session = true
+			opts.auto_session_enabled = true
+			opts.auto_save_enabled = true
+			opts.auto_restore_enabled = true
+			require("auto-session").setup(opts)
+		end
 	end,
 }
