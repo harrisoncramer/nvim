2d88bac3e89c707896bf302414ca7c7dd303235d
Harry Cramer <hcramer@crossbeam.com>
2022-02-07 16:48:14 -0500

    Setup custom easing + speed

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,16 +1,20 @@
 return {
 	setup = function(remap)
+		local t = {}
+		t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } }
+		t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200" } }
+
 		require("neoscroll").setup({
-			mappings = { "<C-u>", "<C-d>", "<C-y>", "<C-e>", "<C-b>", "zt", "zz", "zb" },
 			hide_cursor = true, -- Hide cursor while scrolling
 			stop_eof = true, -- Stop at <EOF> when scrolling downwards
 			use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
 			respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 			cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-			easing_function = nil, -- Default easing function
+			easing_function = "quadratic", -- Default easing function
 			pre_hook = nil, -- Function to run before the scrolling animation starts
 			post_hook = nil, -- Function to run after the scrolling animation ends
 			performance_mode = false, -- Disable "Performance Mode" on all buffers.
 		})
+		require("neoscroll.config").set_mappings(t)
 	end,
 }
