7b267e371bd665b18eb6b4881a9b72a5c50fcf75
Harrison Cramer <harrisoncramer@gmail.com>
2022-02-21 15:36:16 -0500

    Updated Volar TSServer path

--- lua/lsp/servers/volar.lua
+++ lua/lsp/servers/volar.lua
@@ -1,15 +1,6 @@
-local u = require("functions.utils")
 return {
 	setup = function(common_on_attach, capabilities, server)
-		local ts_server
-		if u.get_os() == "Linux" then
-			ts_server =
-				"/home/harrycramer/.local/share/nvim/lsp_servers/volar/node_modules/typescript/lib/tsserverlibrary.js"
-		else
-			ts_server =
-				"/Users/harrisoncramer/.fnm/node-versions/v16.13.0/installation/lib/node_modules/typescript/lib/tsserverlibrary.js"
-		end
-
+		local ts_server = vim.fn.stdpath("data") .. "/lsp_servers/volar/node_modules/typescript/lib/tsserverlibrary.js"
 		local lspconfig = require("lspconfig")
 		server:setup({
 			capabilities = capabilities,
