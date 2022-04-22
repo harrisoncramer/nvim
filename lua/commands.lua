054f3100bfec33be3b3a93851e714e644eef8e32
Harrison Cramer <harrisoncramer@gmail.com>
2022-04-22 14:14:15 -0400

    Updated add_user_command to create_user_command, see https://github.com/neovim/neovim/pull/18071

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,23 +1,23 @@
-vim.api.nvim_add_user_command("RL", function(opts)
+vim.api.nvim_create_user_command("RL", function(opts)
 	require("functions").reload(opts.args)
 end, { nargs = "*" })
 
-vim.api.nvim_add_user_command("RLC", function()
+vim.api.nvim_create_user_command("RLC", function()
 	require("functions").reload_current()
 end, { nargs = 0 })
 
-vim.api.nvim_add_user_command("SC", function()
+vim.api.nvim_create_user_command("SC", function()
 	require("functions").shortcut()
 end, { nargs = 0 })
 
-vim.api.nvim_add_user_command("CAL", function()
+vim.api.nvim_create_user_command("CAL", function()
 	require("functions").calendar()
 end, { nargs = 0 })
 
-vim.api.nvim_add_user_command("Stash", function(opts)
+vim.api.nvim_create_user_command("Stash", function(opts)
 	require("mappings.git").stash(opts.args)
 end, { nargs = 1 })
 
-vim.api.nvim_add_user_command("JQ", function()
+vim.api.nvim_create_user_command("JQ", function()
 	vim.api.nvim_command(".!jq .")
 end, { nargs = 0 })
