e478bf0028e8d5cdd26f68b503f164cb2425a07d
Harrison Cramer <harrisoncramer@gmail.com>
2022-05-08 09:39:31 -0400

    Make BQF appear underneath all splits

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -9,3 +9,8 @@ vim.cmd([[
 vim.cmd([[
   autocmd FileType markdown,text set wrap linebreak nolist textwidth=80 formatoptions=cqt wrapmargin=0
 ]])
+
+vim.cmd([[
+" Position the (global) quickfix window at the very bottom of the window https://github.com/fatih/vim-go/issues/1757
+  autocmd FileType qf if (getwininfo(win_getid())[0].loclist != 1) | wincmd J | endif
+]])
