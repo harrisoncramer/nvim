12fca367ec0209f1b25db044caa27d7196b54973
Harrison Cramer <harrisoncramer@gmail.com>
2022-05-14 09:34:00 -0400

    Removed print lines from null-ls

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -29,9 +29,7 @@ null_ls.setup({
 	debug = false,
 	sources = sources,
 	on_attach = function(client, bufnr)
-		P(client.server_capabilities)
 		if client.server_capabilities.documentFormattingProvider then
-			print("supported")
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
