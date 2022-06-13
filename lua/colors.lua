dbaffb7bd2cf0757faf61789c75b72c4af8cb130
Harrison Cramer <harrisoncramer@gmail.com>
2022-06-13 15:29:53 -0400

    Plugin updates w/ PackerSync

--- lua/colors.lua
+++ lua/colors.lua
@@ -4,23 +4,29 @@ vim.cmd([[ :hi NonText guifg=bg ]])
 
 local kanagawa_ok, kanagawa = pcall(require, "kanagawa")
 if not kanagawa_ok then
-  print("Kanagawa is not installed.")
+	print("Kanagawa is not installed.")
 	return
 end
 
 kanagawa.setup({
-  undercurl = true, -- enable undercurls
-  commentStyle = "italic",
-  functionStyle = "NONE",
-  keywordStyle = "italic",
-  statementStyle = "NONE",
-  typeStyle = "NONE",
-  variablebuiltinStyle = "italic",
-  specialReturn = true, -- special highlight for the return keyword
-  specialException = true, -- special highlight for exception handling keywords
-  transparent = false, -- do not set background color
-  colors = {},
-  overrides = {},
+	undercurl = true, -- enable undercurls
+	commentStyle = {
+		italic = true,
+	},
+	functionStyle = {},
+	keywordStyle = {
+		italic = true,
+	},
+	statementStyle = {},
+	typeStyle = {},
+	variablebuiltinStyle = {
+		italic = true,
+	},
+	specialReturn = true, -- special highlight for the return keyword
+	specialException = true, -- special highlight for exception handling keywords
+	transparent = false, -- do not set background color
+	colors = {},
+	overrides = {},
 })
 vim.api.nvim_exec(
 	[[
