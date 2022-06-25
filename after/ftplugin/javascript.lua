1e9ae3dd154cb29b511676ab94ab4acaa003b077
Harrison Cramer <harrisoncramer@gmail.com>
2022-06-25 18:17:22 -0400

    Added Javascript mappings for map, filter, and reduce

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -20,4 +20,44 @@ vim.keymap.set("n", "<localleader>tfw", function()
 end, map_opts)
 
 -- Javascript specific mappings
--- vim.keymap.set()
+vim.keymap.set("n", "<localleader>(", function()
+	vim.cmd("startinsert")
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("() => {}<left><CR><CR><up><tab>", false, true, true),
+		"n",
+		false
+	)
+end, map_opts)
+
+vim.keymap.set("n", "<localleader>)", function()
+	vim.cmd("startinsert")
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("() => {}<left><CR><CR><up><up><right>", false, true, true),
+		"n",
+		false
+	)
+end, map_opts)
+
+vim.keymap.set("n", "<localleader>m", function()
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("a.map(() => {})<left><left><CR><CR><up><up><ESC>Ei", false, true, true),
+		"n",
+		false
+	)
+end, map_opts)
+
+vim.keymap.set("n", "<localleader>f", function()
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("a.filter(() => {})<left><left><CR><CR><up><up><ESC>Ei", false, true, true),
+		"n",
+		false
+	)
+end, map_opts)
+
+vim.keymap.set("n", "<localleader>r", function()
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("a.reduce((acc, ) => {})<left><left><CR><CR><up><up><ESC>EEi", false, true, true),
+		"n",
+		false
+	)
+end, map_opts)
