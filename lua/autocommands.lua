9a7bc9710706b9ad00f20c60c59a6f1c76931a7c
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-14 09:32:55 -0400

    Set up autoimports for golang

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,16 +1,13 @@
--- Highlight after yank
-vim.cmd([[
-  augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=300 }
-  augroup END
-]])
+-- These are non-language specific autocommands
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+	end,
+	desc = "Highlights the yanked text",
+})
 
+-- Position the (global) quickfix window at the very bottom of the window https://github.com/fatih/vim-go/issues/1757
 vim.cmd([[
-  autocmd FileType markdown,text set wrap linebreak nolist textwidth=80 formatoptions=cqt wrapmargin=0
-]])
-
-vim.cmd([[
-" Position the (global) quickfix window at the very bottom of the window https://github.com/fatih/vim-go/issues/1757
   autocmd FileType qf if (getwininfo(win_getid())[0].loclist != 1) | wincmd J | endif
 ]])
