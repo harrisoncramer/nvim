5b503aaf414669f919d7828e464a3430b75828ed
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-27 09:55:03 -0400

    Fixed keymap for F5

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -102,7 +102,6 @@ return {
 
 		vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint)
 		vim.keymap.set("n", "<leader>bc", dap.step_over)
-		vim.keymap.set("n", "<F5>", dap.continue)
 		-- nnoremap <silent> <F5> <Cmd>lua require'dap'.continue()<CR>
 		-- nnoremap <silent> <F10> <Cmd>lua require'dap'.step_over()<CR>
 		-- nnoremap <silent> <F11> <Cmd>lua require'dap'.step_into()<CR>
