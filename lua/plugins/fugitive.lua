ca0ff9f1844847cc8f900308b7b51409534ed5ae
Harrison Cramer <harrisoncramer@gmail.com>
2022-09-28 09:16:50 -0400

    Made fugitive open in new tab

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,43 +1,46 @@
 local u = require("functions.utils")
 
 local toggle_status = function()
-	local ft = vim.bo.filetype
-	if ft == "fugitive" then
-		vim.api.nvim_command("bd")
-	else
-		vim.api.nvim_command("Git")
-	end
+  local ft = vim.bo.filetype
+  if ft == "fugitive" then
+    vim.api.nvim_command("bd")
+  else
+    vim.api.nvim_command(":Git")
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>T", false, true, true), "n", false)
+    require("plugins.fugitive").jump_next()
+  end
 end
 
 local git_push = function()
-	local isSubmodule = vim.fn.trim(vim.fn.system("git rev-parse --show-superproject-working-tree"))
-	if isSubmodule == "" then
-		if u.get_os() == "Linux" then
-			vim.api.nvim_command("Git push")
-		else
-			vim.api.nvim_command("! git push")
-		end
-	else
-		vim.fn.confirm("Push to origin/main branch for submodule?")
-		vim.api.nvim_command("silent ! git push origin HEAD:main")
-	end
-	local branch = u.get_branch_name()
-	require("notify")("Pushed to '" .. branch .. "' branch")
+  local isSubmodule = vim.fn.trim(vim.fn.system("git rev-parse --show-superproject-working-tree"))
+  if isSubmodule == "" then
+    if u.get_os() == "Linux" then
+      vim.api.nvim_command("Git push")
+    else
+      vim.api.nvim_command("! git push")
+    end
+  else
+    vim.fn.confirm("Push to origin/main branch for submodule?")
+    vim.api.nvim_command("silent ! git push origin HEAD:main")
+  end
+  local branch = u.get_branch_name()
+  require("notify")("Pushed to '" .. branch .. "' branch")
 end
 
 local git_open = function()
-	vim.api.nvim_command("! git open")
+  vim.api.nvim_command("! git open")
 end
 
 local git_mr_open = function()
-	if u.get_os() == "Linux" then
-		os.execute(
-			string.format(
-				"firefox --new-tab 'https://gitlab.com/crossbeam/%s/-/merge_requests?scope=all&state=opened&author_username=hcramer1'",
-				u.current_dir()
-			)
-		)
-	end
+  if u.get_os() == "Linux" then
+    os.execute(
+      string.format(
+        "firefox --new-tab 'https://gitlab.com/crossbeam/%s/-/merge_requests?scope=all&state=opened&author_username=hcramer1'"
+        ,
+        u.current_dir()
+      )
+    )
+  end
 end
 
 vim.keymap.set("n", "<leader>gs", toggle_status, {})
@@ -82,24 +85,24 @@ augroup END
 ]])
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "COMMIT_EDITMSG",
-	callback = function()
-		require("close_buffers").delete({ regex = "^fugitive*" })
-		require("notify")("Commit saved!")
-	end,
+  pattern = "COMMIT_EDITMSG",
+  callback = function()
+    require("close_buffers").delete({ regex = "^fugitive*" })
+    require("notify")("Commit saved!")
+  end,
 })
 
 return {
-	jump_next = function()
-		vim.api.nvim_feedkeys("/^[?,M,A,D,U] ", "n", false)
-		u.press_enter()
-		vim.api.nvim_feedkeys(":noh", "n", false)
-		u.press_enter()
-	end,
-	jump_prev = function()
-		vim.api.nvim_feedkeys("?^[?,M,A,D,U] ", "n", false)
-		u.press_enter()
-		vim.api.nvim_feedkeys(":noh", "n", false)
-		u.press_enter()
-	end,
+  jump_next = function()
+    vim.api.nvim_feedkeys("/^[?,M,A,D,U] ", "n", false)
+    u.press_enter()
+    vim.api.nvim_feedkeys(":noh", "n", false)
+    u.press_enter()
+  end,
+  jump_prev = function()
+    vim.api.nvim_feedkeys("?^[?,M,A,D,U] ", "n", false)
+    u.press_enter()
+    vim.api.nvim_feedkeys(":noh", "n", false)
+    u.press_enter()
+  end,
 }
