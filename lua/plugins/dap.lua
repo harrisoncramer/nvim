ec11e1d4631608ab0950ac51d78c95c788a90a5d
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-02 22:58:23 -0400

    Rearranging windows

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -155,19 +155,22 @@ return {
       layouts = {
         {
           elements = {
-            -- Elements can be strings or table with id and size keys.
-            { id = "scopes", size = 0.25 },
             "breakpoints",
+          },
+          size = 40,
+          position = "right"
+        },
+        {
+          elements = {
             "stacks",
             "watches",
           },
           size = 40, -- 40 columns
-          position = "left",
+          position = "right",
         },
         {
           elements = {
             "repl",
-            "console",
           },
           size = 0.25, -- 25% of total lines
           position = "bottom",
