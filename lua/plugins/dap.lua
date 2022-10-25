3315dc35a47ff68d33bdde6b5680479140ed7055
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-24 21:59:32 -0400

    gutted golang dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -80,52 +80,7 @@ return {
     -- │ Golang                                                   │
     -- ╰──────────────────────────────────────────────────────────╯
 
-    dap.adapters.go = function(callback, config)
-      local stdout = vim.loop.new_pipe(false)
-      local handle
-      local pid_or_err
-      local port = 38697
-      local opts = {
-        stdio = { nil, stdout },
-        args = { "dap", "-l", "127.0.0.1:" .. port },
-        detached = true,
-      }
-      handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-        stdout:close()
-        handle:close()
-        if code ~= 0 then
-          print("dlv exited with code", code)
-        end
-      end)
-      assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-      stdout:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-          vim.schedule(function()
-            require("dap.repl").append(chunk)
-          end)
-        end
-      end)
-      vim.defer_fn(function()
-        callback({ type = "server", host = "127.0.0.1", port = port })
-      end, 100)
-    end
-
-    dap.configurations.go = {
-      {
-        type = "go",
-        name = "Debug",
-        request = "launch",
-        program = "${file}",
-      },
-      {
-        type = "go",
-        name = "Debug test (go.mod)",
-        request = "launch",
-        mode = "test",
-        program = "./${relativeFileDirname}",
-      },
-    }
+    -- Using nvim-dap-go 
 
     vim.keymap.set("n", "<localleader>ds", function()
       require("dapui").toggle()
