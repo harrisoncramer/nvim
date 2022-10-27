f895cee0402373431a92f16756e0fb98eab1fdbc
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-26 21:27:31 -0400

    Fixed eslint errors!

--- lua/lsp/servers/eslint.lua
+++ lua/lsp/servers/eslint.lua
@@ -1,9 +1,15 @@
 -- Requires external install: npm i -g vscode-langservers-extracted
+local root_pattern = require("lspconfig.util").root_pattern
 return {
   setup = function(on_attach, capabilities)
     require("lspconfig").eslint.setup({
       on_attach = on_attach,
       capabilities = capabilities,
+      root_dir = root_pattern(
+        ".eslintrc.js",
+        "node_modules",
+        ".git"
+      ),
     })
   end,
 }
