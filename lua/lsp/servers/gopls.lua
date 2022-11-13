d8ca856c12ffac0c30aa884ce4e2bb1b7b14701a
Harrison Cramer <harrisoncramer@gmail.com>
2022-11-13 17:35:35 -0500

    Fixed issue with nullable actions in gopls

--- lua/lsp/servers/gopls.lua
+++ lua/lsp/servers/gopls.lua
@@ -25,24 +25,29 @@ return {
     if not result or next(result) == nil then
       return
     end
-    local actions = result[1].result
-    if not actions then
-      return
-    end
-    local action = actions[1]
 
-    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-    -- is a CodeAction, it can have either an edit, a command or both. Edits
-    -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-      if action.edit then
-        vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+    for _, val in pairs(result) do
+      local action = val.result[1]
+      if action == nil then
+        goto continue
       end
-      if type(action.command) == "table" then
-        vim.lsp.buf.execute_command(action.command)
+
+      -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+      -- is a CodeAction, it can have either an edit, a command or both. Edits
+      -- should be executed first.
+      if action.edit or type(action.command) == "table" then
+        if action.edit then
+          vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+        end
+        if type(action.command) == "table" then
+          vim.lsp.buf.execute_command(action.command)
+        end
+      else
+        vim.lsp.buf.execute_command(action)
       end
-    else
-      vim.lsp.buf.execute_command(action)
+
+      ::continue::
     end
+
   end,
 }
