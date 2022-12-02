5f11bfe284d482fbe96359b506f3dbe4b2666ad1
Harrison Cramer <harrisoncramer@gmail.com>
2022-12-01 21:24:39 -0500

    Turned off ultisnips mappings

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -1,7 +1,7 @@
 vim.g.UltiSnipsSnippetsDir = "~/.config/nvim/_snippets"
-vim.g.UltiSnipsExpandTrigger = "<C-o>"
-vim.g.UltiSnipsJumpForwardTrigger = "<c-j>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<c-l>"
+-- vim.g.UltiSnipsExpandTrigger = "<C-o>"
+-- vim.g.UltiSnipsJumpForwardTrigger = "<c-j>"
+-- vim.g.UltiSnipsJumpBackwardTrigger = "<c-l>"
 
 vim.cmd([[ let g:UltiSnipsSnippetDirectories = ['_snippets'] ]])
 
