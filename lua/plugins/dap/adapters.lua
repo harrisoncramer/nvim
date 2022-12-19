c3818d73478fdbc79a776151fa32475393ca1bab
Harrison Cramer <harrisoncramer@gmail.com>
2022-12-19 15:06:49 -0800

    Fixed up delve pat

--- lua/plugins/dap/adapters.lua
+++ lua/plugins/dap/adapters.lua
@@ -9,14 +9,14 @@ return {
     dap.adapters.node2 = {
       type = 'executable';
       command = 'node',
-      args = { vim.fn.stdpath "data" .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' };
+      args = { vim.fn.stdpath("data") .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' };
     }
 
     dap.adapters.go = {
       type = "server",
       port = "${port}",
       executable = {
-        command = "dlv",
+        command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
         args = { "dap", "-l", "127.0.0.1:${port}" },
       },
     }
