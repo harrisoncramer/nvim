1f5b881f4822052efe75f8f2bc2ac3ef0d3ce32b
Harrison Cramer <harrisoncramer@gmail.com>
2022-12-21 16:26:41 -0800

    Finished tweaking wilder

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -1,7 +1,50 @@
 return {
   'gelguy/wilder.nvim',
+  dependencies = 'romgrk/fzy-lua-native',
   config = function()
     local wilder = require('wilder')
+    wilder.setup({ modes = { ':', '/', '?' } })
+    local colors = require("colorscheme")
+
+    -- Disable Python remote plugin
+    wilder.set_option('use_python_remote_plugin', 0)
+
+    wilder.set_option('pipeline', {
+      wilder.branch(
+        wilder.cmdline_pipeline({
+          fuzzy = 1,
+          fuzzy_filter = wilder.lua_fzy_filter(),
+        }),
+        wilder.vim_search_pipeline()
+      )
+    })
+
+    wilder.set_option('renderer', wilder.renderer_mux({
+      [':'] = wilder.popupmenu_renderer({
+        highlighter = wilder.lua_fzy_highlighter(),
+        highlights = {
+          accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = colors.peachRed } }),
+        },
+        left = {
+          ' ',
+          wilder.popupmenu_devicons()
+        },
+        right = {
+          ' ',
+          wilder.popupmenu_scrollbar()
+        },
+      }),
+      ['/'] = wilder.popupmenu_renderer({
+        highlighter = wilder.lua_fzy_highlighter(),
+        highlights = {
+          accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = colors.peachRed } }),
+        },
+        right = {
+          ' ',
+          wilder.popupmenu_scrollbar()
+        },
+      }),
+    }))
 
     wilder.setup({
       modes = { ':', '/', '?' },
@@ -9,39 +52,6 @@ return {
       previous_key = '<C-p>',
     })
 
-    wilder.set_option('renderer', wilder.renderer_mux({
-      [":"] = wilder.popupmenu_renderer({
-        highlighter = wilder.basic_highlighter(),
-        left = { " ", wilder.popupmenu_devicons() },
-        right = { " ", wilder.popupmenu_scrollbar() },
-      }),
-      ["/"] = wilder.popupmenu_renderer({
-        highlighter = wilder.basic_highlighter(),
-        right = { " ", wilder.popupmenu_scrollbar() },
-      }),
-    }))
-
-    wilder.set_option('pipeline', {
-      wilder.branch(
-        wilder.cmdline_pipeline({
-          -- sets the language to use, 'vim' and 'python' are supported
-          language = 'python',
-          -- 0 turns off fuzzy matching
-          -- 1 turns on fuzzy matching
-          -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
-          fuzzy = 1,
-        }),
-        wilder.python_search_pipeline({
-          -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-          pattern = wilder.python_fuzzy_pattern(),
-          -- omit to get results in the order they appear in the buffer
-          sorter = wilder.python_difflib_sorter(),
-          -- can be set to 're2' for performance, requires pyre2 to be installed
-          -- see :h wilder#python_search() for more details
-          engine = 're',
-        })
-      ),
-    })
 
   end,
 }
