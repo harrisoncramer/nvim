5a76144320cb6201fac70e05d8923f23e635da3b
Harrison Cramer <harrisoncramer@gmail.com>
2023-01-23 11:24:45 -0500

    Added autocommand to create directory on save if it doesn't exist

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -6,3 +6,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
   desc = "Highlights the yanked text",
 })
+
+-- Taken from: https://stackoverflow.com/questions/4292733/vim-creating-parent-directories-on-save
+vim.cmd([[
+  function s:MkNonExDir(file, buf)
+      if empty(getbufvar(a:buf, '&buftype')) && a:file!~#'\v^\w+\:\/'
+          let dir=fnamemodify(a:file, ':h')
+          if !isdirectory(dir)
+              call mkdir(dir, 'p')
+          endif
+      endif
+  endfunction
+  augroup BWCCreateDir
+      autocmd!
+      autocmd BufWritePre * :call s:MkNonExDir(expand('<afile>'), +expand('<abuf>'))
+  augroup END
+]])
