550f91f7fb6651afdfe9e2de4206d892c465cd3a
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-13 19:55:29 -0600

    Color tweaks

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,12 +1,21 @@
 local function setup()
-  vim.cmd("colorscheme kanagawa")
-
   local kanagawa = require("kanagawa")
+
+  local strongHighlight = "#fa7af6"
 
   kanagawa.setup({
     undercurl = true, -- enable undercurls
     commentStyle = {
       italic = true,
+    },
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
+          }
+        }
+      }
     },
     functionStyle = {},
     keywordStyle = {
@@ -17,6 +26,18 @@ local function setup()
     variablebuiltinStyle = {
       italic = true,
     },
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        IncSearch = { fg = "black", bg = strongHighlight, underline = true, bold = true },
+        Search = { fg = "black", bg = colors.palette.oniViolet },
+        Substitute = { fg = "black", bg = colors.palette.strongHighlight },
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+      }
+    end,
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
     transparent = false, -- do not set background color
@@ -34,6 +55,7 @@ local function setup()
   -- vim.api.nvim_set_hl(0, "@tag.delimiter", { fg = colors.lightBlue, })
   -- vim.api.nvim_set_hl(0, "@tag.attribute", { fg = colors.sakuraPink })
   vim.cmd.hi("NonText guifg=bg")
+  vim.cmd("colorscheme kanagawa")
 end
 
 return { "rebelot/kanagawa.nvim", config = setup }
