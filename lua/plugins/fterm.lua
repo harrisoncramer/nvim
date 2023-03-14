280334d9d01e2ad37c7d71300a7727d5bfdc8a30
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-13 19:39:43 -0600

    Added unception to allow opening files from Fterm

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,6 +1,19 @@
-return { "numToStr/FTerm.nvim", config = function()
-  local fTerm = require("FTerm")
-  vim.keymap.set("n", "<C-z>", fTerm.toggle, {})
-  vim.keymap.set("t", "<C-z>", '<C-\\><C-n>:lua require("FTerm").toggle()<CR>')
-end
+vim.api.nvim_create_autocmd(
+  "User",
+  {
+    pattern = "UnceptionEditRequestReceived",
+    callback = function()
+      -- Toggle the terminal off.
+      require('FTerm').toggle()
+    end
+  }
+)
+
+return {
+  "numToStr/FTerm.nvim",
+  config = function()
+    local fTerm = require("FTerm")
+    vim.keymap.set("n", "<C-z>", fTerm.toggle, {})
+    vim.keymap.set("t", "<C-z>", '<C-\\><C-n>:lua require("FTerm").toggle()<CR>')
+  end
 }
---
+++ lua/plugins/unception.lua
@@ -0,0 +1,6 @@
+return {
+  "samjwill/nvim-unception",
+  init = function()
+    vim.g.unception_open_buffer_in_new_tab = true
+  end
+}
