ddd2e10dbe47a510b4885ea0ff3b2b5248a7bd69
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-17 22:44:25 -0400

    Hiding other annoying notifications 🔔

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,7 +19,7 @@ return {
     "rcarriga/nvim-notify",
   },
   opts = {
-    -- Hide "file written" messages"
+    -- Hide "file written" messages and other garbage notifications
     routes = {
       {
         filter = {
@@ -29,14 +29,62 @@ return {
         },
         opts = { skip = true },
       },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "seconds ago",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "Already at newest change",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "Already at oldest change",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "lines yanked",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "more lines",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
+          event = "msg_show",
+          Hind = "",
+          find = "fewer lines",
+        },
+        opts = { skip = true },
+      }
     },
     format = {
       conceal = true,
     },
     lsp = {
-      signature = {
-        enabled = false
-      },
+      -- signature = {
+      --   enabled = false
+      -- },
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
