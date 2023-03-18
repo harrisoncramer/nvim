ea4494a7cb34bdba043902ef874e1008b7459fae
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-17 21:10:28 -0400

    Added noice for notifications, loading, popups 💅

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,36 @@
+vim.keymap.set("n", "<leader>m", ":messages<CR>")
+
+return {
+  "folke/noice.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    -- Hide "file written" messages"
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
+    },
+    format = {
+      conceal = true,
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    -- Show normal command line
+    cmdline = {
+      view = "cmdline"
+    },
+  },
+}
