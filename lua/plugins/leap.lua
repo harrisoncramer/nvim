b1b5bde27a13996e892f600138388c9d6cf97758
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-17 21:40:43 -0400

    Fixed up noice and fugitive

--- lua/plugins/leap.lua
+++
@@ -1,8 +0,0 @@
-return { 'ggandor/leap.nvim',
-  config = function()
-    require("leap").add_default_mappings()
-    -- Unbind these keymappings for visual mode
-    vim.keymap.del("x", "X")
-    vim.keymap.del("v", "x")
-  end
-}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -22,6 +22,9 @@ return {
       conceal = true,
     },
     lsp = {
+      signature = {
+        enabled = false
+      },
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
