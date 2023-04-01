105f5c2324da10a232f43d3ab4076bb661665787
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-01 14:10:53 -0400

    Added replacement confirmation mapping for quickfix list

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -2,3 +2,5 @@ local map_opts = { noremap = true, silent = true, nowait = true, buffer = true }
 vim.keymap.set("n", "<esc>", ":q<CR>", map_opts)
 vim.keymap.set("n", "<C-n>", "<Down>", map_opts)
 vim.keymap.set("n", "<C-p>", "<Up>", map_opts)
+
+vim.keymap.set("n", "r", ":cdo %s///gc | up<Left><Left><Left><Left><Left><Left><Left><Left><Left>", map_opts)
