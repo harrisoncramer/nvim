a1bd66073c233c5dd8fa4a2782700a24ba48c59c
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-09 13:55:27 -0400

    Added changes

---
+++ lua/plugins/octo.lua
@@ -0,0 +1,11 @@
+return {
+  'pwntester/octo.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'kyazdani42/nvim-web-devicons',
+  },
+  config = {
+
+  }
+}
