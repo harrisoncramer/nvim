83a8a8ee6962d85de58e2ab925a6cd2af1067733
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-13 22:54:52 -0400

    Added tailwind colors to CMP suggestions

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -30,9 +30,32 @@ if cmp_status_ok then
     },
     formatting = {
       format = lspkind.cmp_format({
-        with_text = false, -- do not show text alongside icons
-        maxwidth = 50,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      }),
+        mode = 'symbol_text', -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+        maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        menu = ({             -- showing type in menu
+          nvim_lsp = "[LSP]",
+          path = "[Path]",
+          buffer = "[Buffer]",
+          luasnip = "[LuaSnip]",
+        }),
+        before = function(entry, vim_item) -- for tailwind css autocomplete
+          if vim_item.kind == 'Color' and entry.completion_item.documentation then
+            local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+            if r then
+              local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
+              local group = 'Tw_' .. color
+              if vim.fn.hlID(group) < 1 then
+                vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
+              end
+              vim_item.kind = "ﱢ" -- or "⬤" or anything
+              vim_item.kind_hl_group = group
+              return vim_item
+            end
+          end
+          vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+          return vim_item
+        end
+      })
     },
     mapping = {
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
