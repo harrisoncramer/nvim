2d58b3814d7d6c1dbdbcb9dc29781c473918cd3c
Harrison Cramer <harrisoncramer@gmail.com>
2023-06-20 08:55:39 -0400

    Removed autopairs plugin

--- lua/plugins/autopairs.lua
+++
@@ -1,7 +0,0 @@
-return { "windwp/nvim-autopairs", config = function()
-  require("nvim-autopairs").setup({
-    disable_filetype = { "TelescopePrompt" },
-    enable_check_bracket_line = false,
-  })
-end
-}
--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -1,21 +1,21 @@
 return {
-  "harrisoncramer/gitlab.nvim",
-  dependencies = {
-    "sindrets/diffview.nvim",
-    "rcarriga/nvim-notify",
-    "MunifTanjim/nui.nvim"
-  },
-  build = function()
-    require("gitlab").build()
-  end,
-  dir = "~/.config/nvim/dev-plugins/gitlab",
-  config = function()
-    local gitlab = require("gitlab")
-    vim.keymap.set("n", "<leader>gls", gitlab.summary)
-    vim.keymap.set("n", "<leader>gla", gitlab.approve)
-    vim.keymap.set("n", "<leader>glR", gitlab.revoke)
-    vim.keymap.set("n", "<leader>glc", gitlab.create_comment)
-    vim.keymap.set("n", "<leader>gld", gitlab.list_discussions)
-    gitlab.setup()
-  end,
+  -- "harrisoncramer/gitlab.nvim",
+  -- dependencies = {
+  --   "sindrets/diffview.nvim",
+  --   "rcarriga/nvim-notify",
+  --   "MunifTanjim/nui.nvim"
+  -- },
+  -- build = function()
+  --   require("gitlab").build()
+  -- end,
+  -- dir = "~/.config/nvim/dev-plugins/gitlab",
+  -- config = function()
+  --   local gitlab = require("gitlab")
+  --   vim.keymap.set("n", "<leader>gls", gitlab.summary)
+  --   vim.keymap.set("n", "<leader>gla", gitlab.approve)
+  --   vim.keymap.set("n", "<leader>glR", gitlab.revoke)
+  --   vim.keymap.set("n", "<leader>glc", gitlab.create_comment)
+  --   vim.keymap.set("n", "<leader>gld", gitlab.list_discussions)
+  --   gitlab.setup()
+  -- end,
 }
