d505da44ead3bbf5c683362bbc030e3af6afbb30
Harrison Cramer <harrisoncramer@gmail.com>
2023-08-03 12:33:58 -0400

    Small tweaks

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -63,7 +63,27 @@ return {
         options = {}
       },
       summary = {
-        open = "botright vsplit | vertical resize 60"
+        open = "botright vsplit | vertical resize 60",
+        mappings = {
+          attach = "a",
+          clear_marked = "M",
+          clear_target = "T",
+          debug = "d",
+          debug_marked = "D",
+          expand = { "<CR>", "<2-LeftMouse>" },
+          expand_all = "e",
+          jumpto = "i",
+          mark = "m",
+          next_failed = "J",
+          output = "o",
+          prev_failed = "K",
+          run = "r",
+          run_marked = "R",
+          short = "O",
+          stop = "u",
+          target = "t",
+          watch = "w"
+        },
       },
       highlights = {
         adapter_name = "NeotestAdapterName",
