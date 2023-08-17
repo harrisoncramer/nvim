54d2eb3755a744af739149418cd56cbef425f63c
Harrison Cramer <harrisoncramer@gmail.com>
2023-08-17 10:42:32 -0400

    Updated gitlab config

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -15,11 +15,20 @@ return {
     vim.keymap.set("n", "<leader>glR", gitlab.revoke)
     vim.keymap.set("n", "<leader>glc", gitlab.create_comment)
     vim.keymap.set("n", "<leader>gld", gitlab.list_discussions)
+    vim.keymap.set("n", "<leader>glaa", gitlab.add_assignee)
+    vim.keymap.set("n", "<leader>glad", gitlab.delete_assignee)
+    vim.keymap.set("n", "<leader>glra", gitlab.add_reviewer)
+    vim.keymap.set("n", "<leader>glrd", gitlab.delete_reviewer)
+    vim.keymap.set("n", "<leader>gl", gitlab.delete_reviewer)
     gitlab.setup({
       keymaps = {
         discussion_tree = {
           position = "bottom"
         }
+      },
+      symbols = {
+        resolved = '✓',
+        unresolved = ''
       }
     })
   end,
