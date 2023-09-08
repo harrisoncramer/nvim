1a977b096dc9a6b273425a2c6e46a77dba6a50f1
Harrison Cramer <harrisoncramer@gmail.com>
2023-09-07 20:28:56 -0400

    Added Redir command (:Redir lua=require("something").some_command())

--- lua/commands.lua
+++ lua/commands.lua
@@ -62,6 +62,13 @@ vim.api.nvim_create_user_command("Diff", function(opts)
   vim.api.nvim_feedkeys("sh", "n", false)
 end, { nargs = "*" })
 
+vim.api.nvim_create_user_command('Redir', function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
+  vim.cmd('new')
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = '+', complete = 'command' })
+
 
 vim.cmd([[
   command -nargs=? -bar ReviewChanges call setqflist(map(systemlist("git diff --name-only <args>"), '{"filename": v:val, "lnum": 1}'))
