024213b9c1b40f126b0ebd0807feb60f4a9e68da
Harrison Cramer <harrisoncramer@gmail.com>
2023-09-19 21:49:17 -0400

    Updated plugins

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -2,16 +2,17 @@ return {
   "harrisoncramer/gitlab.nvim",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
   },
   build = function()
-    require("gitlab").build()
+    require("gitlab.server").build()
   end,
   dir = "~/.config/nvim/dev-plugins/gitlab",
   config = function()
     local gitlab = require("gitlab")
     vim.keymap.set("n", "<leader>gls", gitlab.summary)
-    vim.keymap.set("n", "<leader>glo", gitlab.review)
+    vim.keymap.set("n", "<leader>glr", gitlab.review)
+    vim.keymap.set("n", "<leader>glo", gitlab.open_in_browser)
     vim.keymap.set("n", "<leader>glA", gitlab.approve)
     vim.keymap.set("n", "<leader>glR", gitlab.revoke)
     vim.keymap.set("n", "<leader>glc", gitlab.create_comment)
@@ -21,8 +22,10 @@ return {
     vim.keymap.set("n", "<leader>glad", gitlab.delete_assignee)
     vim.keymap.set("n", "<leader>glra", gitlab.add_reviewer)
     vim.keymap.set("n", "<leader>glrd", gitlab.delete_reviewer)
-    vim.keymap.set("n", "<leader>gl", gitlab.delete_reviewer)
+    vim.keymap.set("n", "<leader>glp", gitlab.pipeline)
     gitlab.setup({
+      attachment_dir = "/Users/harrisoncramer/Desktop/screenshots",
+      reviewer = "diffview",
       discussion_tree = {
         position = "bottom",
         blacklist = { "project_7092381_bot_a74db8ad297ab0341e5720af7849e36f" },
