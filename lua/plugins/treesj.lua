692ffb0513d84f38702c7dc6dc86a8bd16d1bc2c
Harrison Cramer <harrisoncramer@gmail.com>
2023-11-13 10:40:13 -0500

    Updated plugins and added treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,8 +1,12 @@
 return {
   'Wansmer/treesj',
-  keys = { '<leader>m', '<leader>j', '<leader>s' },
+  keys = { '<leader>jt', '<leader>jj', '<leader>je' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesj').setup({})
+    local treesj = require('treesj')
+    treesj.setup({ use_deafault_keymaps = false, max_join_length = 1000 })
+    vim.keymap.set('n', "<leader>jj", treesj.toggle)
+    vim.keymap.set('n', "<leader>je", treesj.split)  -- "expand"
+    vim.keymap.set('n', "<leader>jc", treesj.toggle) -- "collapse"
   end,
 }
