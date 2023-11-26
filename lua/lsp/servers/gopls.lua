45093bddec4df62ef77634d274d620e26b9cd59e
Harrison Cramer <harrisoncramer@gmail.com>
2023-11-26 11:37:40 -0500

    Remove comment

--- lua/lsp/servers/gopls.lua
+++ lua/lsp/servers/gopls.lua
@@ -23,11 +23,6 @@ return {
   goimports = function()
     local params = vim.lsp.util.make_range_params()
     params.context = { only = { "source.organizeImports" } }
-    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-    -- machine and codebase, you may want longer. Add an additional
-    -- argument after params if you find that you have to write the file
-    -- twice for changes to be saved.
-    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
     for cid, res in pairs(result or {}) do
       for _, r in pairs(res.result or {}) do
