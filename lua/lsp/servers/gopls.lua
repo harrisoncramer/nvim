c0de509e9f31e2e5c5094eeffcc0c97c0c97c5e6
Harrison Cramer <harrisoncramer@gmail.com>
2023-11-26 11:36:59 -0500

    Fix go imports

--- lua/lsp/servers/gopls.lua
+++ lua/lsp/servers/gopls.lua
@@ -20,44 +20,23 @@ return {
       }
     })
   end,
-  goimports = function(timeout_ms)
-    local context = { source = { organizeImports = true } }
-    vim.validate({ context = { context, "t", true } })
+  goimports = function()
     local params = vim.lsp.util.make_range_params()
-    params.context = context
-
-    -- See the implementation of the textDocument/codeAction callback
-    -- (lua/vim/lsp/handler.lua) for how to do this properly.
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result or next(result) == nil then
-      return
-    end
-
-    for _, val in pairs(result) do
-      if val.result == nil then
-        goto continue
-      end
-      local action = val.result[1]
-      if action == nil then
-        goto continue
-      end
-
-      -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-      -- is a CodeAction, it can have either an edit, a command or both. Edits
-      -- should be executed first.
-      if action.edit or type(action.command) == "table" then
-        if action.edit then
-          vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+    params.context = { only = { "source.organizeImports" } }
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
         end
-        if type(action.command) == "table" then
-          vim.lsp.buf.execute_command(action.command)
-        end
-      else
-        vim.lsp.buf.execute_command(action)
       end
-
-      ::continue::
     end
-
+    vim.lsp.buf.format({ async = false })
   end,
 }
