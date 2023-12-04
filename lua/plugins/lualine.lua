df8487ea1656cb97beed4407ce0d1c14ad6707c9
Harrison Cramer <harrisoncramer@gmail.com>
2023-12-04 18:30:44 -0500

    Changed up lualine to winbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,56 +1,64 @@
-local function get_git_head()
-  local head = vim.fn.FugitiveHead()
-  if head == "" or head == nil then
-    return "DETATCHED "
-  end
-  if string.len(head) > 20 then
-    head = ".." .. head:sub(15)
-  end
-  return " " .. head
-end
-
 return {
   "nvim-lualine/lualine.nvim",
   requires = { "kyazdani42/nvim-web-devicons", opt = true },
   config = function()
     require("lualine").setup({
       options = {
-        component_separators = { right = "" },
+        component_separators = { right = "" },
         section_separators = { left = "", right = "" },
         theme = "kanagawa",
+        globalstatus = true,
       },
       sections = {
-        lualine_a = { get_git_head },
-        lualine_b = { "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filetype",
-            colored = true,   -- Displays filetype icon in color if set to true
-            icon_only = true, -- Display only an icon for filetype
-          },
-          {
-            "filename",
-            file_status = true,
-            path = 1,
-            symbols = {
-              modified = "  ",    -- Text to show when the file is modified.
-              readonly = "[-]",      -- Text to show when the file is non-modifiable or readonly.
-              unnamed = "[No Name]", -- Text to show for unnamed buffers.
-            },
-          },
+        lualine_a = { 'branch' },
+        lualine_b = {
+          require("recorder").recordingStatus
+        },
+        lualine_c = { 'diagnostics' },
+        lualine_d = {
           {
             require("recorder").recordingStatus
           },
           {
             require("recorder").displaySlots
           },
+          -- lualine_z = {},
         },
-        lualine_d = {
-        },
-        lualine_w = {},
-        lualine_x = {},
-        lualine_y = {},
+        lualine_x = { 'diff' },
+        lualine_y = { 'encoding', 'filetype' },
         lualine_z = {},
+      },
+      inactive_winbar = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            path = 1,
+            symbols = {
+              modified = "  ",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+            },
+          }
+        },
+      },
+      winbar = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            path = 1,
+            symbols = {
+              modified = "  ",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+            },
+          }
+        },
+        lualine_b = {},
+        lualine_c = { 'diagnostics' },
+        lualine_x = { 'diff' },
+        lualine_y = { 'progress', 'location' }
       },
     })
   end
