78fa0f4c79f504d66052b847d1cca65734a2bf8e
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-11 10:00:27 -0500

    Updated plugins

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -16,7 +16,7 @@ return {
         -- fullscreen = true,
         preview = {
           layout   = 'vertical',
-          vertical = 'up:45%', -- up|down:size
+          vertical = 'up:70%', -- up|down:size
         }
       },
       actions = {
@@ -53,7 +53,7 @@ return {
     })
     vim.keymap.set("n", "<C-j>", fzfLua.git_files)
     vim.keymap.set("n", "<C-m>", function()
-      fzfLua.git_files({ cmd = "git ls-files -mu --format='%(path)'" })
+      fzfLua.git_files({ cmd = "git diff --name-only main" })
     end, {})
     vim.keymap.set("n", "<C-f>", fzfLua.live_grep_native)
     vim.keymap.set("n", "<leader>tgc", fzfLua.git_commits, {})
