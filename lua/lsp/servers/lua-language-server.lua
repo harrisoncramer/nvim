fd7ce1942e93761a93f5395ba715d9111e77b155
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-24 12:21:14 -0500

    Fix LSP for Lua for real

--- lua/lsp/servers/lua-language-server.lua
+++ lua/lsp/servers/lua-language-server.lua
@@ -10,42 +10,30 @@ return {
     require("lspconfig").lua_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
-      on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-                -- Setup your lua path
-                path = vim.split(package.path, ";"),
-              },
-              diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {
-                  "vim",
-                  "nnoremap",
-                  "vnoremap",
-                  "inoremap",
-                  "tnoremap",
-                  "use",
-                },
-              },
-              workspace = {
-                -- Make the server aware of Neovim runtime files
-                checkThirdParty = false,
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
-              },
-            }
-          })
-          client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        end
-        return true
-      end,
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {
+              'vim',
+              'require'
+            },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      }
     })
   end,
 }
