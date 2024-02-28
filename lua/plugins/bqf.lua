16fa7f4998f525f225f70cd289ff714f56bed3bf
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-28 09:57:34 -0500

    Updated lua/plugins/bqf.lua

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -15,39 +15,9 @@ local function setup()
   require("bqf").setup({
     border = 'rounded',
     preview = {
+      description = "Make BQF opaque",
       winblend = 0,
     },
-    filter = {
-      fzf = {
-        action_for = {
-          ['ctrl-q'] = {
-            description = [[Press ctrl-q to toggle sign for the selected items]],
-            default = 'signtoggle'
-          },
-          ['ctrl-t'] = {
-            description = [[Press ctrl-t to open up the item in a new tab]],
-            default = 'tabedit'
-          },
-          ['ctrl-v'] = {
-            description = [[Press ctrl-v to open up the item in a new vertical split]],
-            default = 'vsplit'
-          },
-          ['ctrl-x'] = {
-            description = [[Press ctrl-x to open up the item in a new horizontal split]],
-            default = 'split'
-          },
-          ['ctrl-c'] = {
-            description = [[Press ctrl-c to close quickfix window and abort fzf]],
-            default = 'closeall'
-          }
-        },
-        -- This is not working
-        -- extra_opts = {
-        --   description = 'Extra options for fzf',
-        --   default = { '--bind', 'ctrl-s:select-all' }
-        -- }
-      }
-    }
   })
 end
 
