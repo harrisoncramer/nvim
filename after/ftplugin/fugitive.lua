b8c6d0e7b43e415c877b7e3dfd93235cb2128417
Harrison Cramer <harrisoncramer@gmail.com>
2024-04-12 10:00:00 -0400

    Remove unused fugitive bindings

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -11,19 +11,9 @@ vim.cmd([[
       exe 'tabedit ' . a:filename
       Gdiffsplit
   endfunction
-
-  " custom mapping in fugitive window (:Git)
-  augroup custom_fugitive_mappings
-      au!
-      au User FugitiveIndex nnoremap <buffer> <C-n> :lua require("plugins.fugitive").jump_next()<CR>
-      au User FugitiveIndex nnoremap <buffer> <C-p> :lua require("plugins.fugitive").jump_prev()<CR>
-      au User FugitiveIndex nnoremap <buffer> sj <C-w>j
-  augroup END
 ]])
 
 vim.keymap.set("n", "df", ":call GdiffsplitTab(GStatusGetFilenameUnderCursor())<cr>", map_opts)
 vim.keymap.set("n", "cc", ":silent! Git commit --quiet<CR>", map_opts)
 vim.keymap.set("n", "ca", ":silent! Git commit --amend --quiet<CR>", map_opts)
-vim.keymap.set("n", "<C-n>", require("plugins.fugitive").jump_next, map_opts)
-vim.keymap.set("n", "<C-p>", require("plugins.fugitive").jump_prev, map_opts)
 vim.keymap.set("n", "gr", ":silent! Git reset --soft HEAD~1<CR>", map_opts)
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,19 +1,5 @@
 local u = require("functions.utils")
 
-local function jump_next()
-  vim.api.nvim_feedkeys(":silent! /^[?,M,A,D,U] ", "n", false)
-  u.press_enter()
-  vim.api.nvim_feedkeys(":noh", "n", false)
-  u.press_enter()
-end
-
-local function jump_prev()
-  vim.api.nvim_feedkeys(":silent! ?^[?,M,A,D,U] ", "n", false)
-  u.press_enter()
-  vim.api.nvim_feedkeys(":noh", "n", false)
-  u.press_enter()
-end
-
 local toggle_status = function()
   local ft = vim.bo.filetype
   if ft == "fugitive" then
@@ -25,7 +11,6 @@ local toggle_status = function()
     end
     vim.api.nvim_command("silent! :Git")
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>T", false, true, true), "n", false)
-    jump_next()
   end
 end
 
@@ -35,7 +20,4 @@ return {
     local map_opts = { noremap = true, silent = true, nowait = true, buffer = false }
     vim.keymap.set("n", "<leader>gs", toggle_status, map_opts)
   end,
-  dependencies = { "nvim-lua/plenary.nvim" },
-  jump_next = jump_next,
-  jump_prev = jump_prev,
 }
