b75f108e6049a1458b7947a6c74e957d865944e9
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-11 22:23:06 -0400

    Removed old keybindings for gitlab

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -7,32 +7,9 @@ return {
   build = function()
     require("gitlab.server").build()
   end,
-  dir = "~/.config/nvim/dev-plugins/gitlab",
+  -- dir = "~/.config/nvim/dev-plugins/gitlab",
   config = function()
     local gitlab = require("gitlab")
-    vim.keymap.set("n", "gls", gitlab.summary)
-    vim.keymap.set("n", "glr", gitlab.review)
-    vim.keymap.set("n", "glo", gitlab.open_in_browser)
-    vim.keymap.set("n", "glA", gitlab.approve)
-    vim.keymap.set("n", "glR", gitlab.revoke)
-    vim.keymap.set("n", "glc", gitlab.create_comment)
-    vim.keymap.set("v", "glc", gitlab.create_multiline_comment)
-    vim.keymap.set("n", "gln", gitlab.create_note)
-    -- vim.keymap.set("v", "gln", gitlab.create_comment_suggestion)
-    vim.keymap.set("n", "gld", gitlab.toggle_discussions)
-    vim.keymap.set("n", "glaa", gitlab.add_assignee)
-    vim.keymap.set("n", "glad", gitlab.delete_assignee)
-    vim.keymap.set("n", "glra", gitlab.add_reviewer)
-    vim.keymap.set("n", "glrd", gitlab.delete_reviewer)
-    vim.keymap.set("n", "glp", gitlab.pipeline)
-    vim.keymap.set("n", "glm", gitlab.move_to_discussion_tree_from_diagnostic)
-    vim.keymap.set("n", "glM", gitlab.merge)
-    vim.keymap.set("n", "glO", gitlab.create_mr)
-    vim.keymap.set("n", "glla", gitlab.add_label)
-    vim.keymap.set("n", "glld", gitlab.delete_label)
-    vim.keymap.set("n", "glu", gitlab.copy_mr_url)
-    vim.keymap.set("n", "glb", gitlab.choose_merge_request)
-
     gitlab.setup({
       attachment_dir = "/Users/harrisoncramer/Desktop/screenshots",
       debug = { go_request = true, go_response = true },
@@ -45,9 +22,14 @@ return {
         temporary_registers = { '"', "+", "g" },
       },
       keymaps = {
-        reviewer = {
-          create_suggestion = "x"
-        }
+        global = {
+          start_review = "glr",
+          start_review_nowait = true,
+        },
+        discussion_tree = {
+          switch_view = "S",
+          switch_view_nowait = true,
+        },
       },
       create_mr = {
         target = "main",
