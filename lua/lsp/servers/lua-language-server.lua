b3ffae28f1cc41ed3607744327cc06e37226f89b
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-16 21:34:50 -0400

    Updated lua/lsp/servers/lua-language-server.lua

--- lua/lsp/servers/lua-language-server.lua
+++ lua/lsp/servers/lua-language-server.lua
@@ -27,6 +27,7 @@ return {
           workspace = {
             -- Make the server aware of Neovim runtime files
             library = vim.api.nvim_get_runtime_file("", true),
+            ignoreDir = { "node_modules" }
           },
           -- Do not send telemetry data containing a randomized but unique identifier
           telemetry = {
