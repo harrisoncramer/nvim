24919542f6c07eeef97d10832e67094153471316
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-24 18:05:11 -0400

    Removed Harpoon

--- lua/plugins/harpoon.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'ThePrimeagen/harpoon',
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
-    vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
-  end
-}
