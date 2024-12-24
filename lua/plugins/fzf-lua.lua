7fb62cf07ab9f506347fc37fd2cb58dac80e0721
Harrison Cramer <harrisoncramer@gmail.com>
2024-12-23 22:06:43 -0500

    Fix: BQF and FZF Integration
    Simplifies some keybindings for FZF, BQF, and some of the search and
    grep workflows. Removes unused macro workflows.

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,10 +1,3 @@
-local u = require("functions.utils")
-
-local find_matching_files = function()
-  local bare_file_name = u.return_bare_file_name()
-  require("fzf-lua").git_files({ search = bare_file_name, no_esc = true })
-end
-
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -13,7 +6,6 @@ return {
     local actions = require("fzf-lua.actions")
     fzfLua.setup({
       winopts = {
-        -- fullscreen = true,
         preview = {
           layout   = 'vertical',
           vertical = 'up:70%', -- up|down:size
@@ -24,6 +16,7 @@ return {
           ["default"] = actions.file_edit_or_qf,
           ["ctrl-q"]  = actions.file_sel_to_qf,
           ["ctrl-v"]  = actions.file_vsplit,
+          ["ctrl-t"]  = actions.file_tabedit,
         }
       },
       fzf_opts = {
@@ -44,22 +37,25 @@ return {
       },
       keymap = {
         fzf = {
+          false,
           ["ctrl-z"] = "abort",
-          ["ctrl-s"] = "select-all",
-          ["f3"] = "toggle-preview-wrap",
-          ["f4"] = "toggle-preview",
+          ["ctrl-o"] = "select-all",
+          -- TODO: Want to change to <C-u> and <C-d> but they aren't working
           ["shift-down"] = "preview-page-down",
           ["shift-up"] = "preview-page-up",
         },
       }
     })
     vim.keymap.set("n", "<C-j>", fzfLua.git_files)
+    vim.keymap.set("n", "<C-g><C-j>", function()
+      fzfLua.git_files({ resume = true })
+    end)
     vim.keymap.set("n", "<C-m>", function()
       fzfLua.git_files({ cmd = "git diff --name-only main && git ls-files --exclude-standard --others" })
     end, {})
     vim.keymap.set("n", "<C-f>", fzfLua.live_grep_native)
-    vim.keymap.set("n", "<leader>tgc", fzfLua.git_commits, {})
-    vim.keymap.set("n", "<leader>tgb", fzfLua.git_branches, {})
-    vim.keymap.set("n", "<leader>tj", find_matching_files)
+    vim.keymap.set("n", "<C-g><C-f>", function()
+      fzfLua.live_grep_native({ resume = true })
+    end)
   end
 }
--- lua/plugins/nvim-recorder.lua
+++ lua/plugins/nvim-recorder.lua
@@ -1,126 +1,13 @@
-local popup = require("plenary.popup")
-local u = require("functions.utils")
-
-vim.keymap.set("n", "<leader>qo", function() MyMenu() end, {}) -- Show macros
-
-local function extract_chunk_after_gap(input_string)
-  local chunk = input_string:match("[a-g]%s*(.+)")
-  return chunk
-end
-
-local window_id
-function ShowMenu(opts, cb)
-  local height = 20
-  local width = 100
-  local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-
-  local content = {}
-  local array_length = #opts
-
-  -- Take each macro line and turn into a table
-  for i, value in ipairs(opts) do
-    if i < array_length then
-      local new_string = value:gsub("\n", "")
-      local str = new_string:find('"')
-      if str then new_string = new_string:sub(str + 1) end
-      table.insert(content, new_string)
-    end
-  end
-
-
-  window_id = popup.create(content, {
-    title = "Macros",
-    highlight = "MyProjectWindow",
-    line = math.floor(((vim.o.lines - height) / 2) - 1),
-    col = math.floor((vim.o.columns - width) / 2),
-    minwidth = width,
-    minheight = height,
-    borderchars = borderchars,
-    callback = cb,
-  })
-
-  local function closeMenu()
-    vim.api.nvim_win_close(window_id, true)
-  end
-
-  local function editMacro()
-    local line_content = u.get_line_content()
-    local macro = extract_chunk_after_gap(line_content)
-    local register = line_content:sub(1, 1)
-
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
-    vim.api.nvim_buf_set_lines(0, 0, 1, false, { macro })
-
-    vim.keymap.set('n', '<leader>s', function()
-      local line_content_updated = u.get_line_content()
-      vim.fn.setreg(register, line_content_updated)
-      closeMenu()
-      require("notify")("Macro saved!")
-    end, { silent = true, buffer = 0 })
-  end
-
-
-  vim.keymap.set('n', '<Esc>', function()
-    closeMenu()
-  end, { silent = true, buffer = 0 })
-
-  vim.keymap.set('n', 'e', function()
-    editMacro()
-  end, { silent = true, buffer = 0 })
-end
-
-local function get_multiple_registers_content(registers)
-  local reg_content_array = {}
-  local saved_reg_content = vim.fn.getreg('@')
-
-  for i, register in ipairs(registers) do
-    vim.cmd('redir => reg_content')
-    vim.cmd('silent execute "reg ' .. register .. '"')
-    vim.cmd('redir END')
-    local reg_content = vim.fn.eval('reg_content')
-
-    -- Remove header
-    if reg_content ~= nil then
-      for s in reg_content:gmatch("[^\r\n]+") do
-        if s ~= 'Type Name Content' then
-          reg_content_array[i] = s
-        end
-      end
-    end
-  end
-
-  vim.fn.setreg('@', saved_reg_content)
-  return reg_content_array
-end
-
-local function get_register(input_string)
-  local character_before_blank = input_string:match("([a-g])%s")
-  return character_before_blank
-end
-
-local registers = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } -- The macro slots and registers
-function MyMenu()
-  local opts = get_multiple_registers_content(registers)
-  local cb = function(_, sel)
-    local macro_register = get_register(sel)
-    local cmd = '@' .. macro_register
-    vim.api.nvim_feedkeys(cmd, 'n', true)
-  end
-  ShowMenu(opts, cb)
-end
-
 return {
   "chrisgrieser/nvim-recorder",
-  dependencies = "rcarriga/nvim-notify", -- optional
+  dependencies = "rcarriga/nvim-notify",
   opts = {
-    slots = registers,
+    slots = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' },
     mapping = {
       startStopRecording = "q",
       playMacro = "Q",
-      editMacro = "<leader>qe",
-      switchSlot = "<leader>qt",
     },
-    lessNotifications = true,
+    lessNotifications = false,
     clear = false,
     logLevel = vim.log.levels.INFO,
     dapSharedKeymaps = false,
