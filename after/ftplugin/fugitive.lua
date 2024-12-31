81cf4ba1e3158f61cb0a5b8a99288967f07cd73b
Harrison Cramer <harrisoncramer@gmail.com>
2024-12-31 10:36:20 -0800

    Fixed keybindings

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,16 +1,17 @@
 local map_opts = { noremap = true, silent = true, nowait = true, buffer = true }
 local u = require("functions.utils")
 
--- See changes in this specific commit
-vim.keymap.set("n", "<CR>", function()
+-- See diff of changes in this specific commit
+vim.keymap.set("n", "d", function()
   local sha = u.get_word_under_cursor()
   vim.print(sha)
   vim.api.nvim_feedkeys(string.format(":DiffviewOpen %s~1..%s", sha, sha), "n", false)
   u.press_enter()
 end, map_opts)
 
--- See all changes against origin in this commit
-vim.keymap.set("n", "<Shift><CR>", function()
+-- See diff of all changes against origin in this commit
+vim.keymap.set("n", "D", function()
+  print("hi")
   local sha = u.get_word_under_cursor()
   vim.print(sha)
   vim.api.nvim_feedkeys(string.format(":DiffviewOpen origin/main...%s", sha), "n", false)
