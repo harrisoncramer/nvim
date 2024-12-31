1351a22f3399bc13d13e9e6d85d0d392c5d3906e
Harrison Cramer <harrisoncramer@gmail.com>
2024-12-31 10:33:01 -0800

    Updated after/ftplugin/fugitive.lua

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,8 +1,16 @@
 local map_opts = { noremap = true, silent = true, nowait = true, buffer = true }
 local u = require("functions.utils")
 
--- vim.keymap.del("n", "<CR>")
+-- See changes in this specific commit
 vim.keymap.set("n", "<CR>", function()
+  local sha = u.get_word_under_cursor()
+  vim.print(sha)
+  vim.api.nvim_feedkeys(string.format(":DiffviewOpen %s~1..%s", sha, sha), "n", false)
+  u.press_enter()
+end, map_opts)
+
+-- See all changes against origin in this commit
+vim.keymap.set("n", "<Shift><CR>", function()
   local sha = u.get_word_under_cursor()
   vim.print(sha)
   vim.api.nvim_feedkeys(string.format(":DiffviewOpen origin/main...%s", sha), "n", false)
