431ef474ad124e51d52212aa0e6d0a6d442ad4cc
Harry Cramer <harrisonc@givechariot.com>
2025-01-10 20:16:27 -0500

    Updated fugitive

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,6 +1,14 @@
 local map_opts = { noremap = true, silent = true, nowait = true, buffer = true }
 local u = require("functions.utils")
 
+vim.keymap.set("n", "<esc>", ":q<CR>", map_opts)
+vim.keymap.set("n", "cc", function()
+  vim.cmd(":Git commit --quiet")
+end, map_opts)
+vim.keymap.set("n", "ca", function()
+  vim.cmd(":Git commit --quiet --amend")
+end, map_opts)
+
 -- See diff of changes in this specific commit
 vim.keymap.set("n", "d", function()
   local sha = u.get_word_under_cursor()
