b0ff22c97b4a731eca6974234d7e6e7cce3a3542
Harrison Cramer <harrisoncramer@gmail.com>
2025-01-19 16:44:19 -0500

    Setting up keybindings for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,22 @@ return {
   dependencies = 'rafamadriz/friendly-snippets',
   version = '*',
   opts = {
-    keymap = { preset = 'default' },
+    signature = {},
+    keymap = {
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'select_and_accept' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
+      ['<C-d>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-u>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+    },
+    completion = {
+      documentation = {
+        auto_show = true,
+      }
+    },
     appearance = {
       nerd_font_variant = 'mono'
     },
