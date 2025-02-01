6e060ec1133df2da00489d0a856d759e6e893fc9
Harry Cramer <harrisonc@givechariot.com>
2025-02-01 12:47:48 -0500

    Fixed msg type

--- lua/functions/work.lua
+++ lua/functions/work.lua
@@ -3,7 +3,7 @@ local M = {}
 vim.api.nvim_create_user_command("FOLLOW", function(opts)
   local result = vim.fn.system(string.format("watch %s", opts.args))
   if vim.v.shell_error ~= 0 then
-    require("notify")(string.format("Could not follow '%s' container: ", opts.args) .. result)
+    require("notify")(string.format("Could not follow '%s' container: ", opts.args) .. result, vim.log.levels.ERROR)
   end
 end, { nargs = 1 })
 
