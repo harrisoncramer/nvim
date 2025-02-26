31cbcffb67d03dd08f241dd82705947bcdf9bcd3
Harrison Cramer <harrisoncramer@gmail.com>
2025-02-25 23:18:09 -0500

    Staged changes to gitlinker

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,15 +1,27 @@
+local map_opts = { noremap = true, silent = false, nowait = true, buffer = false }
+
+vim.keymap.set("n", "yg", function()
+	require("gitlinker.actions").copy_to_clipboard()
+end, map_opts)
+
 return {
 	"ruifm/gitlinker.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	config = function()
 		require("gitlinker").setup({
 			opts = {
+				remote = nil,
+				add_current_line_on_normal_mode = true,
+				-- callback for what to do with the url
+				action_callback = require("gitlinker.actions").copy_to_clipboard,
+				-- print the url after performing the action
 				print_url = false,
-				action_callback = function(...)
-					require("gitlinker.actions").copy_to_clipboard(...)
-					vim.notify("Copied location to clipboard")
-				end,
 			},
+			callbacks = {
+				["github.com"] = require("gitlinker.hosts").get_github_type_url,
+				["gitlab.com"] = require("gitlinker.hosts").get_gitlab_type_url,
+			},
+			mappings = "gy",
 		})
 	end,
 }
