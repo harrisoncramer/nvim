40bc6b313bf1f971c5dabfd83a4b1eabf4654de6
Harry Cramer <harrisonc@givechariot.com>
2025-03-01 11:36:57 -0500

    Updates query for SQL commands

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -21,41 +21,13 @@ local function add_json_tag()
 	vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, false, { new_line })
 end
 
+-- Go to the SQL query for the word under the cursor
 local function go_to_query()
 	local word = u.get_word_under_cursor()
-
-	local cmd = string.format(
-		"rg -o -t sql --json --line-number --column \"%s\" . | jq -r 'select(.data.submatches | length > 0)' | jq -r '.data'",
-		word
-	)
-	local j = io.popen(cmd)
-	if j == nil then
-		require("notify")("No query found", vim.log.levels.ERROR)
-		return
-	end
-
-	local json = vim.fn.trim(j:read("*a"))
-	local success, data = pcall(vim.json.decode, json)
-	if not success then
-		require("notify")("Error decoding JSON: " .. tostring(data), vim.log.levels.ERROR)
-		return
-	end
-
-	if
-		not data
-		or not data.path
-		or not data.path.text
-		or not data.line_number
-		or not data.submatches
-		or not data.submatches[1]
-		or not data.submatches[1].start
-	then
-		require("notify")("Invalid data structure in JSON", vim.log.levels.ERROR)
-		return
-	end
-
-	vim.cmd("edit " .. data.path.text) -- Open the file specified in the path
-	vim.fn.cursor(data.line_number, data.submatches[1].start) -- Move cursor to the line and column
+	require("functions.search-word").go_to_word({
+		word = word,
+		filetype = "sql",
+	})
 end
 
 vim.keymap.set("n", "<localleader>jt", add_json_tag)
---
+++ lua/functions/search-word.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+-- Search for the word under the cursor
+M.go_to_word = function(opts)
+	opts = opts or {}
+	local filetype_option = opts.filetype and ("-t " .. opts.filetype) or ""
+	local cmd = string.format(
+		"rg -o %s --json --line-number --column \"%s\" . | jq -s 'map(select(.data.submatches | length > 0) | .data)'",
+		filetype_option,
+		opts.word
+	)
+
+	local j, err = io.popen(cmd)
+	if j == nil then
+		require("notify")("Error running command: " .. tostring(err), vim.log.levels.ERROR)
+		return
+	end
+
+	local json = vim.fn.trim(j:read("*a"))
+	if json == "" then
+		require("notify")("Word not found", vim.log.levels.ERROR)
+		return
+	end
+
+	local success, data = pcall(vim.json.decode, json)
+	if not success then
+		require("notify")("Error decoding JSON: " .. tostring(data), vim.log.levels.ERROR)
+		return
+	end
+
+	if vim.tbl_isempty(data) then
+		require("notify")("Search is empty", vim.log.levels.WARN)
+		return
+	end
+
+	if #data == 1 then
+		local item = data[1]
+		vim.cmd("edit " .. item.path.text) -- Open the file specified in the path
+		vim.fn.cursor(item.line_number, item.submatches[1].start) -- Move cursor to the line and column
+	else
+		local qf_list = {}
+		for _, item in ipairs(data) do
+			table.insert(qf_list, {
+				filename = item.path.text,
+				lnum = item.line_number,
+				col = item.submatches[1].start,
+				text = "Match",
+			})
+		end
+		vim.fn.setqflist(qf_list, "r")
+		vim.cmd("copen")
+	end
+end
+
+return M
