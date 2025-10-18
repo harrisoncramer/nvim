0d8e8cee6cb2fa8e5ce6d503ca305a6ed4bc88f8
Harrison Cramer <harrisoncramer@gmail.com>
2025-10-18 12:31:28 -0400

    Fixed up Diffview keybindings.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,3 @@
--- Gets the hash
 local M = {
 	"sindrets/diffview.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
@@ -6,14 +5,29 @@ local M = {
 		vim.opt.fillchars:append({ diff = "╱" })
 		local actions = require("diffview.actions")
 		local diffview = require("diffview")
-		local cb = require("diffview.config").diffview_callback
 		local git_helpers = require("git-helpers")
 		local github = require("git-helpers.github")
 
 		-- Diffview changes. Can be used to stage/unstage files.
-		vim.keymap.set("n", "<leader>gdd", function()
-			vim.cmd("DiffviewOpen")
-		end, merge(global_keymap_opts, { desc = "Diffview all changes" }))
+		vim.keymap.set("n", "<leader>gs", function()
+			local current_tab = vim.api.nvim_get_current_tabpage()
+			local windows = vim.api.nvim_tabpage_list_wins(current_tab)
+			local in_diffview = false
+			for _, win in ipairs(windows) do
+				local buf = vim.api.nvim_win_get_buf(win)
+				local buf_name = vim.api.nvim_buf_get_name(buf)
+				if buf_name:match("DiffviewFilePanel") then
+					in_diffview = true
+					break
+				end
+			end
+
+			if in_diffview then
+				vim.cmd("DiffviewClose")
+			else
+				vim.cmd("DiffviewOpen")
+			end
+		end, merge(global_keymap_opts, { desc = "Toggle Diffview" }))
 
 		-- View entire changes versus a specific branch.
 		vim.keymap.set("n", "<leader>gdD", function()
@@ -85,77 +99,311 @@ local M = {
 				-- The `view` bindings are active in the diff buffers, only when the current
 				-- tabpage is a Diffview.
 				view = {
-					["cc"] = function()
-						vim.cmd("tabclose")
-						vim.cmd("Git commit")
-					end,
-					["<C-n>"] = cb("select_next_entry"), -- Open the diff for the next file
-					["<C-p>"] = cb("select_prev_entry"), -- Open the diff for the previous file
-					["<CR>"] = cb("goto_file_edit"), -- Open the file in a new split in previous tabpage
-					["<C-w><C-f>"] = cb("goto_file_split"), -- Open the file in a new split
-					["<C-w>gf"] = cb("goto_file_tab"), -- Open the file in a new tabpage
-					["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
-					["<leader>b"] = cb("toggle_files"), -- Toggle the files panel.
-				},
-				file_panel = {
-					["cc"] = function()
-						vim.cmd("tabclose")
-						vim.cmd("Git commit")
-					end,
-					["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
-					["<down>"] = cb("next_entry"),
-					["k"] = cb("prev_entry"), -- Bring the cursor to the previous file entry.
-					["<up>"] = cb("prev_entry"),
-					["o"] = cb("select_entry"),
-					["<2-LeftMouse>"] = cb("select_entry"),
 					{
 						"n",
-						"s",
+						"cc",
+						function()
+							vim.cmd("tabclose")
+							vim.cmd("Git commit")
+						end,
+						{ desc = "Close diffview and commit" },
+					},
+					{
+						"n",
+						"<C-n>",
+						actions.select_next_entry,
+						{ desc = "Open the diff for the next file" },
+					},
+					{
+						"n",
+						"<C-p>",
+						actions.select_prev_entry,
+						{ desc = "Open the diff for the previous file" },
+					},
+					{
+						"n",
+						"<CR>",
+						actions.goto_file_edit,
+						{ desc = "Open the file in a new split in previous tabpage" },
+					},
+					{
+						"n",
+						"<C-w><C-f>",
+						actions.goto_file_split,
+						{ desc = "Open the file in a new split" },
+					},
+					{
+						"n",
+						"<C-w>gf",
+						actions.goto_file_tab,
+						{ desc = "Open the file in a new tabpage" },
+					},
+					{
+						"n",
+						"<leader>e",
+						actions.focus_files,
+						{ desc = "Bring focus to the files panel" },
+					},
+					{
+						"n",
+						"<leader>b",
+						actions.toggle_files,
+						{ desc = "Toggle the files panel" },
+					},
+				},
+				file_panel = {
+					{
+						"n",
+						"cc",
+						function()
+							vim.cmd("tabclose")
+							vim.cmd("Git commit")
+						end,
+						{ desc = "Close diffview and commit" },
+					},
+					{
+						"n",
+						"j",
+						actions.next_entry,
+						{ desc = "Bring the cursor to the next file entry" },
+					},
+					{
+						"n",
+						"<down>",
+						actions.next_entry,
+						{ desc = "Bring the cursor to the next file entry" },
+					},
+					{
+						"n",
+						"k",
+						actions.prev_entry,
+						{ desc = "Bring the cursor to the previous file entry" },
+					},
+					{
+						"n",
+						"<up>",
+						actions.prev_entry,
+						{ desc = "Bring the cursor to the previous file entry" },
+					},
+					{
+						"n",
+						"o",
+						actions.select_entry,
+						{ desc = "Select entry" },
+					},
+					{
+						"n",
+						"<2-LeftMouse>",
+						actions.select_entry,
+						{ desc = "Select entry" },
+					},
+					{
+						"n",
+						"-",
 						actions.toggle_stage_entry,
 						{ desc = "Stage / unstage the selected entry" },
 					},
-					["S"] = cb("stage_all"), -- Stage all entries.
-					["U"] = cb("unstage_all"), -- Unstage all entries.
-					["X"] = cb("restore_entry"), -- Restore entry to the state on the left side.
-					["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
-					["<S-Up>"] = actions.scroll_view(-20),
-					["<S-Down>"] = actions.scroll_view(20),
-					["<C-n>"] = cb("select_next_entry"),
-					["<C-p>"] = cb("select_prev_entry"),
-					["gf"] = cb("goto_file"),
-					["<cr>"] = cb("goto_file_tab"),
-					["i"] = cb("listing_style"), -- Toggle between 'list' and 'tree' views
-					["f"] = cb("toggle_flatten_dirs"), -- Flatten empty subdirectories in tree listing style.
-					["<leader>e"] = cb("focus_files"),
+					{
+						"n",
+						"S",
+						actions.stage_all,
+						{ desc = "Stage all entries" },
+					},
+					{
+						"n",
+						"U",
+						actions.unstage_all,
+						{ desc = "Unstage all entries" },
+					},
+					{
+						"n",
+						"X",
+						actions.restore_entry,
+						{ desc = "Restore entry to the state on the left side" },
+					},
+					{
+						"n",
+						"R",
+						actions.refresh_files,
+						{ desc = "Update stats and entries in the file list" },
+					},
+					{
+						"n",
+						"<S-Up>",
+						actions.scroll_view(-20),
+						{ desc = "Scroll view up" },
+					},
+					{
+						"n",
+						"<S-Down>",
+						actions.scroll_view(20),
+						{ desc = "Scroll view down" },
+					},
+					{
+						"n",
+						"<C-n>",
+						actions.select_next_entry,
+						{ desc = "Select next entry" },
+					},
+					{
+						"n",
+						"<C-p>",
+						actions.select_prev_entry,
+						{ desc = "Select previous entry" },
+					},
+					{
+						"n",
+						"gf",
+						actions.goto_file,
+						{ desc = "Go to file" },
+					},
+					{
+						"n",
+						"<cr>",
+						actions.goto_file_tab,
+						{ desc = "Go to file in new tab" },
+					},
+					{
+						"n",
+						"i",
+						actions.listing_style,
+						{ desc = "Toggle between 'list' and 'tree' views" },
+					},
+					{
+						"n",
+						"f",
+						actions.toggle_flatten_dirs,
+						{ desc = "Flatten empty subdirectories in tree listing style" },
+					},
+					{
+						"n",
+						"<leader>e",
+						actions.focus_files,
+						{ desc = "Focus files panel" },
+					},
 				},
 				file_history_panel = {
-					["g!"] = cb("options"), -- Open the option panel
-					["<C-A-d>"] = cb("open_in_diffview"), -- Open the entry under the cursor in a diffview
-					["zR"] = cb("open_all_folds"),
-					["zM"] = cb("close_all_folds"),
-					["j"] = cb("next_entry"),
-					["<down>"] = cb("next_entry"),
-					["k"] = cb("prev_entry"),
-					["<up>"] = cb("prev_entry"),
-					["<cr>"] = cb("select_entry"),
-					["o"] = cb("select_entry"),
-					["<2-LeftMouse>"] = cb("select_entry"),
-					["<C-n>"] = cb("select_next_entry"),
-					["<C-p>"] = cb("select_prev_entry"),
-					["gf"] = cb("goto_file"),
-					["<C-w><C-f>"] = cb("goto_file_split"),
-					["<C-w>gf"] = cb("goto_file_tab"),
-					["<leader>e"] = cb("focus_files"),
-					["<leader>b"] = cb("toggle_files"),
-					["<leader>go"] = function()
-						local hash = git_helpers.get_hash()
-						if hash == nil then
-							return
-						end
-						github.open_pr_from_hash(hash)
-					end,
+					{
+						"n",
+						"g!",
+						actions.options,
+						{ desc = "Open the option panel" },
+					},
+					{
+						"n",
+						"<C-A-d>",
+						actions.open_in_diffview,
+						{ desc = "Open the entry under the cursor in a diffview" },
+					},
+					{
+						"n",
+						"zR",
+						actions.open_all_folds,
+						{ desc = "Open all folds" },
+					},
+					{
+						"n",
+						"zM",
+						actions.close_all_folds,
+						{ desc = "Close all folds" },
+					},
+					{
+						"n",
+						"j",
+						actions.next_entry,
+						{ desc = "Next entry" },
+					},
+					{
+						"n",
+						"<down>",
+						actions.next_entry,
+						{ desc = "Next entry" },
+					},
+					{
+						"n",
+						"k",
+						actions.prev_entry,
+						{ desc = "Previous entry" },
+					},
+					{
+						"n",
+						"<up>",
+						actions.prev_entry,
+						{ desc = "Previous entry" },
+					},
+					{
+						"n",
+						"<cr>",
+						actions.select_entry,
+						{ desc = "Select entry" },
+					},
+					{
+						"n",
+						"o",
+						actions.select_entry,
+						{ desc = "Select entry" },
+					},
+					{
+						"n",
+						"<2-LeftMouse>",
+						actions.select_entry,
+						{ desc = "Select entry" },
+					},
+					{
+						"n",
+						"<C-n>",
+						actions.select_next_entry,
+						{ desc = "Select next entry" },
+					},
+					{
+						"n",
+						"<C-p>",
+						actions.select_prev_entry,
+						{ desc = "Select previous entry" },
+					},
+					{
+						"n",
+						"gf",
+						actions.goto_file,
+						{ desc = "Go to file" },
+					},
+					{
+						"n",
+						"<C-w><C-f>",
+						actions.goto_file_split,
+						{ desc = "Go to file in split" },
+					},
+					{
+						"n",
+						"<C-w>gf",
+						actions.goto_file_tab,
+						{ desc = "Go to file in new tab" },
+					},
+					{
+						"n",
+						"<leader>e",
+						actions.focus_files,
+						{ desc = "Focus files panel" },
+					},
+					{
+						"n",
+						"<leader>b",
+						actions.toggle_files,
+						{ desc = "Toggle files panel" },
+					},
+					{
+						"n",
+						"<leader>go",
+						function()
+							local hash = git_helpers.get_hash()
+							if hash == nil then
+								return
+							end
+							github.open_pr_from_hash(hash)
+						end,
+						{ desc = "Open PR from hash" },
+					},
 				},
-				option_panel = { ["<tab>"] = cb("select"), ["q"] = cb("close") },
 			},
 		})
 	end,
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,15 +1,4 @@
-local toggle_status = function()
-	local ft = vim.bo.filetype
-	if ft == "fugitive" then
-		vim.cmd("bd")
-	else
-		vim.cmd("silent! Git")
-	end
-end
-
 return {
 	"tpope/vim-fugitive",
-	init = function()
-		vim.keymap.set("n", "<leader>gs", toggle_status, merge(global_keymap_opts, { desc = "Toggle git status" }))
-	end,
+	init = function() end,
 }
