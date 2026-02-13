eed422082c4d089552baa4f0008010a44a73f905
Harrison Cramer <harrisoncramer@gmail.com>
2026-02-13 09:37:19 -0500

    Updated lua/claude-helpers/init.lua

--- lua/claude-helpers/init.lua
+++ lua/claude-helpers/init.lua
@@ -1,12 +1,6 @@
 vim.keymap.set(
 	"n",
-	"<C-a><C-i>",
-	require("claude-helpers.linear").enrich_issue,
-	vim.tbl_extend("force", global_keymap_opts, { desc = "Linear investigate" })
+	"<C-l>",
+	require("claude-helpers.linear").select_issue,
+	vim.tbl_extend("force", global_keymap_opts, { desc = "Select Linear issue" })
 )
-
-vim.keymap.set("n", "<C-a><C-r>", function()
-	require("git-helpers").branch_input(function(branch)
-		require("claude-helpers.review").review_changes(branch)
-	end)
-end, merge(global_keymap_opts, { desc = "Send diff of current branch to code companion" }))
