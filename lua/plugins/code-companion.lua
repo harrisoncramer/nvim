85caa3def25cc79c9773ea3d7ab490346f85f31a
Harrison Cramer <harrisoncramer@gmail.com>
2026-02-17 10:04:44 -0500

    Updated lua/plugins/code-companion.lua

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -10,7 +10,7 @@ end
 
 vim.keymap.set("n", "<C-a><C-r>", function()
 	require("git-helpers").branch_input(function(branch)
-		require("claude-helpers.review").review_changes(branch)
+		require("claude").review_changes(branch)
 	end)
 end, merge(global_keymap_opts, { desc = "Send diff of current branch to code companion" }))
 
