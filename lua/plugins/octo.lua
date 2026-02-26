9ddf8226273f5febdd96f0b59ce83394c6984ee0
Harrison Cramer <harrisoncramer@gmail.com>
2026-02-26 09:41:09 -0500

    Updated lua/plugins/octo.lua

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -12,7 +12,7 @@ return {
 		require("octo").setup({
 			picker = "snacks",
 			use_local_fs = true,
-			enable_builtin = false, -- shows a list of builtin actions when no action is provided
+			enable_builtin = true, -- shows a list of builtin actions when no action is provided
 			default_remote = { "upstream", "origin" },
 			default_merge_method = "merge", -- default merge method which should be used for both `Octo pr merge` and merging from picker, could be `merge`, `rebase` or `squash`
 			default_delete_branch = false, -- whether to delete branch when merging pull request with either `Octo pr merge` or from picker (can be overridden with `delete`/`nodelete` argument to `Octo pr merge`)
